0d817f1b5b165753e829f099003b871399c13c69
AnuragKashyap999 <anuragkashyap701153@gmail.com>
2026-06-04 01:49:44 +0530

    Write auth methods like login , logOut , getCurrentUser etc.

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,6 +1,57 @@
-import { Config } from "tailwindcss/plugin";
+import config from "../config/config";
+import  Config  from "../config/config";
 import {Client , Account ,ID} from "appwrite";
 
-export class AuthService{}
+export class AuthService{
+    client = new Client();
+    account;
+    constructor(){
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.account=new Account(this.client)
+    }
 
-export default AuthService;
+    async createAccount({eamil,password,name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(),eamil,password,name);
+            if(userAccount){
+               // call another method 
+               return this.login({eamil,password})
+            }else{
+                return userAccount
+            }
+        } catch (error) {
+         throw error;   
+        }
+    }
+
+    async login({eamil,password}){
+        try{
+           return await this.account.createEmailPasswordSession(eamil,password);
+        }catch(error){
+           throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+          return await this.account.get()
+        } catch (error) {
+           console.log("Appwrite service :: getCurrentUser :: error",error)
+        }
+        return null;
+    }
+
+    async logOut(){
+        try {
+            return await this.account.deleteSessions()
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error",error)
+        }
+    }
+}
+   
+const authService = new AuthService();
+
+export default authService;
