50a7c676126641e09c678a35bcc3b5ac22364483
AnuragKashyap999 <anuragkashyap701153@gmail.com>
2026-06-15 23:12:27 +0530

    Make reusable components input and button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {Container,Logo,LogoutBtn } from '../index.js'
-import { Link } from 'react-router-dom'
+import { Link} from 'react-router-dom'
 import {useSelector} from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 
 function Header() {
   const authStatus = useSelector((state)=>state.auth.status)
-  const useNavigate = useNavigate()
+  const navigate = useNavigate()
+
   const navItems=[
     {
       name:'Home',
@@ -46,6 +47,19 @@ function Header() {
         </div>
         <ul className='flex ml-auto'>
           {navItems.map((item)=>
+          item.active ? (
+            <li key={item.name}>
+              <button onClick={()=>navigate(item.slug)}
+              className='inline-block px-6 py-2
+              duration-200 hover:bg-blue-100 rounded-full'
+              >{item.name}</button>
+            </li>
+          ) : null
+          )}
+          {authStatus && (
+            <li>
+              <LogoutBtn/>
+            </li>
           )}
 
         </ul>
