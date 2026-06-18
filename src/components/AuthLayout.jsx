fa3a178701d19a354c49aadb0c8fdb42f92b0c0f
AnuragKashyap999 <anuragkashyap701153@gmail.com>
2026-06-18 22:51:55 +0530

    add authLayout, RTE component, Postform

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React ,{useState,useEffect}from 'react'
+import {useSelector} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
 
-export default function AuthLayout() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function Protected({children,authentication = true}){
+  const navigate = useNavigate()
+  const [loader,setLoader] = useState(true)
+
+  const authStatus = useSelector(state=>state.auth.status)
+
+  useEffect(()=>{
+    // TODO : make it more easy to understand 
+   
+    if(authentication && authStatus!==authentication){
+      navigate('/login')
+    }else if(!authentication && authStatus!==authentication){
+      navigate('/')
+    }
+
+     // second way 
+    // if(authStatus===true){
+    //   navigate('/');
+    // }else if (authStatus===false){
+    //   navigate('/login')
+    // }
+    
+    setLoader(false)
+  },[authStatus,navigate,authentication])
+  return loader? <h1>Loading...</h1> : <>{children}</>
 }
---
+++ src/components/Post-form/Postform.jsx
@@ -0,0 +1,17 @@
+import React ,{useCallback} from 'react'
+import {useForm} from 'react-hook-form'
+import {Button,Input,Select , RTE} from '../index'
+import appwriteService from '../../appwrite/conf'
+import { useNavigate } from 'react-router-dom'
+import {useSelector} from 'react-redux'
+
+
+function Postform() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Postform
