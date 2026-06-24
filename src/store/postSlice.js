159eb2731434fc467e6a3e8b227b53ca69355ff4
AnuragKashyap999 <anuragkashyap701153@gmail.com>
2026-06-24 22:14:34 +0530

    write a code of  postSlice

---
+++ src/store/postSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState 
+const Postslice = createSlice({
+    name:'post',
+    initialState,
+    reducers:{
+
+    }
+})
+
+export default Postslice.reducer
