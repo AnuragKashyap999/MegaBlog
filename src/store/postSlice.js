17c741b80fa7b34f00e397993c2d13b8cb13348e
AnuragKashyap999 <anuragkashyap701153@gmail.com>
2026-06-25 23:29:50 +0530

    write code  add post reducer in Postslice file

--- src/store/postSlice.js
+++ src/store/postSlice.js
@@ -1,12 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState 
+const initialState  ={
+    posts:[],
+    loading:false
+}
 const Postslice = createSlice({
     name:'post',
     initialState,
     reducers:{
-
+        addPost:(state,action)=>{
+            state.posts.push(action.payload)
+        },
     }
 })
+
+
 
 export default Postslice.reducer
